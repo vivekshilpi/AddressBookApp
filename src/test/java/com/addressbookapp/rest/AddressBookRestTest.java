9b41026b2ea86d46d0d2257788c74f312c9169dc
vivekshilpi <vivekshilpi1234@gmail.com>
2026-03-10 01:53:09 +0530

    [Vivek] : Added UC23 feature add contact jsonserver

--- src/test/java/com/addressbookapp/rest/AddressBookRestTest.java
+++ src/test/java/com/addressbookapp/rest/AddressBookRestTest.java
@@ -2,6 +2,7 @@ package com.addressbookapp.rest;
 
 import com.addressbookapp.model.Contact;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +34,38 @@ public class AddressBookRestTest {
         assertTrue(contacts.size() > 0);
 
         contacts.forEach(System.out::println);
+    }
+    
+    // ---- UC 23 ----
+    
+    @Test
+    public void givenNewContact_whenAdded_shouldReturn201() {
+
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3000;
+
+        Contact contact = new Contact(
+                "Rahul",
+                "Verma",
+                "Indore Address",
+                "Indore",
+                "MP",
+                "452001",
+                "7777777777",
+                "rahul@test.com"
+        );
+
+        Response response =
+                given()
+                .contentType(ContentType.JSON)
+                .body(contact)
+                .when()
+                .post("/contacts");
+
+        int statusCode = response.getStatusCode();
+
+        assertEquals(201, statusCode);
+
+        System.out.println(response.getBody().asString());
     }
 }
